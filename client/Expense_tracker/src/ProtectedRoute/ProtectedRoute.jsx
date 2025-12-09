d31a2bb50dbeb5d95ddbfc61c29760112e9f6006
Ram Sharma <sharmashekhar20050@gmail.com>
2025-12-09 15:24:09 +0545

    Update authentication API endpoint URL

--- client/Expense_tracker/src/ProtectedRoute/ProtectedRoute.jsx
+++ client/Expense_tracker/src/ProtectedRoute/ProtectedRoute.jsx
@@ -8,7 +8,7 @@ const ProtectedRoute = ({ children }) => {
   useEffect(() => {
     const checkAuth = async () => {
       try {
-        const res = await axios.get("https://mern-expense-tracker-1-frontend.onrender.com/auth/", {
+        const res = await axios.get("https://mern-expense-tracker-g93r.onrender.com/auth/", {
           withCredentials: true,
         });
         console.log(res);
