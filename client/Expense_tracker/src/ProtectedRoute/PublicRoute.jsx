176b1e3d1ef21e62092f073b8588577f5193ce72
Ram Sharma <sharmashekhar20050@gmail.com>
2025-12-09 15:19:00 +0545

    Update authentication API endpoint to production URL

--- client/Expense_tracker/src/ProtectedRoute/PublicRoute.jsx
+++ client/Expense_tracker/src/ProtectedRoute/PublicRoute.jsx
@@ -8,7 +8,7 @@ const PublicRoute = ({ children }) => {
   useEffect(() => {
     const checkAuth = async () => {
       try {
-        const res = await axios.get("http://localhost:5000/auth/", {
+        const res = await axios.get("https://mern-expense-tracker-1-frontend.onrender.com/auth/", {
           withCredentials: true,
         });
         if (res.data.userID) setIsAuthenticated(true);
