d38574e7455aa98b826f2673e34910a8d0a29899
Ram Sharma <sharmashekhar20050@gmail.com>
2025-12-13 17:22:35 +0545

    Update OTP email sender to use environment variables

--- server/utils/otpSender.js
+++ server/utils/otpSender.js
@@ -6,13 +6,13 @@ export const sendOTP = (email, otp) => {
    host:'smtp-relay.brevo.com',
     port:587,
     auth: {
-      user: "sharmashekhar20050@gmail.com",
-      pass: process.env.GMAIL_PASS,
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
     },
   });
   //creating mail options
   const mailOptions = {
-    from: "sharmashekhar20050@gmail.com",
+    from: process.env.SENDER_EMAIL,
     to: email,
     subject: "Forget your password 🔑",
     html: `${otpHTML(otp)}`,
