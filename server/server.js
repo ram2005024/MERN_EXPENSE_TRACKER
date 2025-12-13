8f2b8170c93b5b6e172c4ce48b0d441602588ca7
Ram Sharma <sharmashekhar20050@gmail.com>
2025-12-13 16:11:18 +0545

    Update CORS origin for frontend URL

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://mern-expense-tracker-2-frontend-final.onrender.com",
+    origin: "https://mern-expense-tracker-2-frontend.onrender.com",
     credentials: true,
   })
 );
