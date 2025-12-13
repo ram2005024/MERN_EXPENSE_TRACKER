353d5aaec1450140423db82b8262a938482b88fa
Ram Sharma <sharmashekhar20050@gmail.com>
2025-12-13 18:43:36 +0545

    Final Commit

--- server/utils/otpSender.js
+++ server/utils/otpSender.js
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendOTP = async (email, otp) => {
   try {
     await resend.emails.send({
-      from: "Experience Tracker <sharmashekhar20050@gmail.com>",
+      from: "Experience Tracker <onboarding@resend.dev>",
       to: email,
       subject: "Reset your password 🔐",
       html: otpHTML(otp),
@@ -14,6 +14,6 @@ export const sendOTP = async (email, otp) => {
 
     console.log("OTP email sent successfully");
   } catch (error) {
-    console.error("Resend email failed:", error);
+    console.error("Resend error:", error);
   }
 };
