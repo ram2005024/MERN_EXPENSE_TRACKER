30b604b26810548291f998517d8de21f515b677a
Ram Sharma <sharmashekhar20050@gmail.com>
2025-12-13 12:54:53 +0545

    Fix conditional check for authentication sections

--- client/Expense_tracker/src/pages/Authentication.jsx
+++ client/Expense_tracker/src/pages/Authentication.jsx
@@ -80,7 +80,7 @@ const Authentication = () => {
     }
   };
   useEffect(() => {
-    if (section === "login" && section === "signup") {
+    if (section === "login" && section === "signup" && section==="forget) {
       setEmail("");
       setPwd("");
       setUserName("");
